78466525db9294bee08a9b976233859c2f06d0d4
Baety48 <awuorlizzbetty1@gmail.com>
2022-09-09 22:37:02 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,14 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int ch;
 
-	for (ch = 'a' ; ch <= 'z' : ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 		putchar(ch);
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
-		putchar(ch)
+		putchar(ch);
 	putchar('\n');
 	return (0);
+}
