9e8fdd68f30775539a8ae3f0857f67fd52dce0a4
Baety48 <awuorlizzbetty1@gmail.com>
2022-09-14 12:04:30 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - print putchar
+ * followed by a new line
+ * Return: 0
+ */
+
 int main(void)
 
 {
 	printf("_putchar\n");
 
-	return 0;
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/Readme.md
@@ -0,0 +1,1 @@
+Today we are learning about nested loops
